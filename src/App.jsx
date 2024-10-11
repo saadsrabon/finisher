aa54b192a72050030144c384737130a16ea328e4
Saad Srabon <saadsrabon2@gmail.com>
2024-10-12 00:44:17 +0600

    Tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,16 @@
-
-
-import HeroSection from './components/HeroSection'
-import Items from './components/Items'
-import Navbar from './components/Navbar'
-import { TasksProvider } from './context/TaskContext'
-function App() {
-  
-  return (
-    <>
-      {/* <!-- Navbar Starts --> */}
- <Navbar />
-	<section className="mb-20" id="tasks">
-		
-		<div className="container">
-           <HeroSection/>
-		{/* <!-- Search Box Ends --> */}
-
-		<TasksProvider>
-			<Items/>
-			</TasksProvider>
-		</div>
-	</section>
-	{/* <!-- End Table --> */}
-
-	{/* <!-- Footer --> */}
-	<footer className="py-6 md:py-8">
-		<div className="container mx-auto">
-			<p className="text-center text-xs text-[#6A6A6A] lg:text-sm">
-				Copyright ©2024 | All rights reserved by Learn with Sumit
-			</p>
-		</div>
-	</footer>
-    </>
-  )
+import Footer from "./Footer";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import TaskBoard from "./task/TaskBoard";
+export default function App() {
+    return (
+        <>
+            <Header />
+            <div className="flex flex-col justify-center items-center">
+                <HeroSection />
+                <TaskBoard />
+            </div>
+            <Footer />
+        </>
+    );
 }
-
-export default App
